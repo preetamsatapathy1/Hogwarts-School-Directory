e5c8808aec9ff4dd738e9346f737ee7b85e1b0b4
Preetam Satapathy <preetam.satapathy1@gmail.com>
2020-10-24 23:14:04 +0530

    version 1.0

---
+++ search.js
@@ -0,0 +1,43 @@
+const searchItem = document.getElementById('search');
+const charContainer = document.getElementById('character-list')
+
+searchItem.addEventListener('keyup',getSearchInput)
+
+let actors = [];
+async function fetchData(){
+    try{
+        let res = await fetch('http://hp-api.herokuapp.com/api/characters')
+        actors = await res.json()
+        showData(actors)
+    }
+    catch(err){
+        console.log(err)
+    } 
+}
+
+function getSearchInput (e) {
+    let input = e.target.value
+    let filteredItem = actors.filter(actor => {
+        return (actor.name.toLowerCase().includes(input.toLowerCase()))
+    })
+    showData(filteredItem)
+}
+
+function showData(dataList){
+    let item = dataList.map(data => {
+        return(
+        `<li class="item">
+            <div id="data">
+                <h2> ${data.name}</h2>
+                <p>House: ${data.house}</p>
+            </div>
+            <img src = ${data.image}>
+        </li>`)
+    })
+    let htmlData = item.join("")
+    charContainer.innerHTML = htmlData
+    
+}
+
+fetchData()
+
